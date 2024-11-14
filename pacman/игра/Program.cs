7f87d09827fbf53ef3e5c05dc185e11a0da8f7f6
Btlz1 <bealtes0@gmail.com>
2024-11-14 14:07:08 +0500

    add

---
+++ pacman/игра/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace игра
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            char[,] map =
+            {
+    {'#','#','#','#','#','#','#','#','#','#','#','#' },
+    {'#','x','#',' ',' ',' ',' ',' ',' ','x',' ','#' },
+    {'#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+    {'#',' ','#','x',' ',' ',' ',' ',' ',' ',' ','#' },
+    {'#',' ','#',' ',' ','#','#','#','#',' ',' ','#' },
+    {'#',' ','#',' ',' ','#',' ',' ',' ',' ',' ','#' },
+    {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#' },
+    {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#' },
+    {'#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+    {'#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+    {'#',' ','#',' ',' ',' ',' ',' ',' ','x',' ','#' },
+    {'#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+    {'#','x','#',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+    {'#','#','#','#','#','#','#','#','#','#','#','#' }
+};
+
+            int userX = 6; int userY = 6;
+            char[] bag = new char[1];
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, 20);
+                Console.Write("сумка: ");
+                for (int i = 0; i < bag.Length; i++)
+                {
+                    Console.Write(bag[i] + " ");
+                }
+
+
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.SetCursorPosition(userY, userX);
+                Console.WriteLine("@");
+                ConsoleKeyInfo charKey = Console.ReadKey();
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (map[userX - 1, userY] != '#')
+                        {
+                            userX--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (map[userX + 1, userY] != '#')
+                        {
+                            userX++;
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (map[userX, userY - 1] != '#')
+                            userY--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (map[userX, userY + 1] != '#')
+                            userY++;
+                        break;
+                }
+
+                if (map[userX, userY] == 'x')
+                {
+                    map[userX, userY] = ' ';
+                    char[] tempBag = new char[bag.Length + 1];
+                    for (int i = 0; i < bag.Length; i++)
+                    {
+                        tempBag[i] = bag[i];
+                    }
+                    tempBag[tempBag.Length - 1] = 'x';
+                    bag = tempBag;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
